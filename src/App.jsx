e0f7d2f3d1b0ea1115249b092d4464acd566f528
Jeevan Aaron <jeevan.aaron.joseph@gmail.com>
2023-02-13 00:23:39 +0530

    fetches pokemon names using axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,30 @@
-import { useState } from 'react'
+import { useState ,useEffect} from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import axios from 'axios'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [cards, setCards] = useState([])
+
+  useEffect (() => {
+    axios.get('https://pokeapi.co/api/v2/pokemon')
+    .then(res => {
+      console.log(res.data)
+      setCards(res.data.results);
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  } , []
+  )
 
   return (
-    <div className="App">
-      
+    <div >
+      <ul>
+        {
+          cards.map( result => <li>{result.name}</li>)
+        }
+      </ul>
     </div>
   )
 }
